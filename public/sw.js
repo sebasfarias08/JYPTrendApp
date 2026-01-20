e1e1b26e7ceadb544cf095effc5edd658a05c669
Sebastian Farias <sebastian.h.farias@gmail.com>
2026-01-20 15:56:29 +0000

    Correccion PWD no funciona

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 /* public/sw.js */
-const APP_VERSION = "v1.0.0"; // subilo cuando publiques cambios importantes
+const APP_VERSION = "v1.0.1"; // subilo cuando publiques cambios importantes
 const CACHE_STATIC = `static-${APP_VERSION}`;
 const CACHE_RUNTIME = `runtime-${APP_VERSION}`;
 
