c0b32570e97ecafe743c5a474e8099858b5abe09
Sebastian Farias <sebastian.h.farias@gmail.com>
2026-01-20 19:40:59 +0000

    Sprint 3 Ajustes de login

--- public/js/supabase-client.js
+++ public/js/supabase-client.js
@@ -3,7 +3,11 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const SUPABASE_URL = "https://fkfipbrevepkztcmnfgi.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_3NtZaq1zpVMsylskqSUmIQ_83xXQJts";
 
-export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    flowType: "pkce",
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true
+  }
+});
